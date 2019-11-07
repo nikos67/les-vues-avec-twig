05080e57a0e689469d0b1f8ae22e9a719a84f9b8
Nicolas <nbrunstein67@gmail.com>
2019-11-07 11:02:09 +0100

    first commit

---
+++ public/index.php
@@ -0,0 +1,6 @@
+<?php
+require '../vendor/autoload.php';
+$loader = new Twig\Loader\FilesystemLoader('../View');
+$twig = new Twig\Environment($loader);
+$products = ['product1', 'product2', 'product3', 'product4', 'product5'];
+echo $twig->render('index.html.twig', ['products' => $products]);
